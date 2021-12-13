34b65fb1854ad434efa1d91cce76449441ddab97
mukul0000 <mukulrajput9675@gmail.com>
2021-12-13 15:15:18 +0530

    kepp working

--- src/components/user.jsx
+++ src/components/user.jsx
@@ -1,7 +1,34 @@
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+import { color } from "@mui/system";
 export const User = () => {
 	return (
 		<>
-			<h1>working</h1>
+			<h1>sign up</h1>
+			<br />
+			<form>
+				<TextField
+					helperText=" "
+					id="demo-helper-text-aligned"
+					label="First_Name"
+				/>
+				<br />
+				<TextField
+					helperText=""
+					id="demo-helper-text-aligned-no-helper"
+					label="Last_Name"
+				/>
+				<br />
+				<br />
+				<TextField
+					helperText=""
+					id="demo-helper-text-aligned-no-helper"
+					label="OTP"
+				/>
+				<br />
+				<br />
+				<Button style={{ border: "2px solid black" }}>sign up</Button>
+			</form>
 		</>
 	);
 };
